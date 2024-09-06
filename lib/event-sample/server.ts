97b6a34416c8b900f67e75495fdcc5161bad114a
Justin Martin <justin@kezios.fr>
2024-09-06 22:04:34 +0200

    cleaning

--- lib/event-sample/server.ts
+++
@@ -1,8 +0,0 @@
-import { triggerServerEventPromise } from "@lib/event/client";
-
-const result = await triggerServerEventPromise({
-  event: "myEvent",
-  params: { name: "John" },
-});
-
-console.log(result); // { name: "John" }
