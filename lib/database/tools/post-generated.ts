80f30d338d45edfb0cbb6196f4a24c0792a23f99
JustinMartinDev <justin.martin.developper@gmail.com>
2024-09-07 00:45:27 +0200

    add post generated code

---
+++ lib/database/tools/post-generated.ts
@@ -0,0 +1,35 @@
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+
+const toReplace = `
+  const alternativePath = alternativePaths.find((altPath) => {
+    return fs.existsSync(path.join(process.cwd(), altPath, 'schema.prisma'))
+  }) ?? alternativePaths[0]
+
+  config.dirname = path.join(process.cwd(), alternativePath)
+  config.isBundled = true
+`;
+
+const replaceValue = `
+  const alternativePath = alternativePaths.find((altPath) => {
+    return fs.existsSync(path.join(process.cwd(), altPath, 'schema.prisma'))
+  }); 
+
+  if(alternativePath) {
+    config.dirname = path.join(process.cwd(), alternativePath)
+  } else if(config?.generator?.output?.value) {
+    config.dirname = config.generator.output.value;
+  } else {
+    config.dirname = path.join(process.cwd(), alternativePaths[0]);
+  }
+
+  config.isBundled = true  
+`;
+
+const file = join(process.cwd(), 'prisma', 'generated', 'index.js');
+
+const indexContent = readFileSync(file, { encoding: 'utf-8'});
+
+const newIndexContent = indexContent.replace(toReplace, replaceValue);
+
+writeFileSync(file, newIndexContent);
