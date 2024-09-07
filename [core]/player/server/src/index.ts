d51aa902504b97c85b36c7cc5e7ea859888a742d
JustinMartinDev <justin.martin.developper@gmail.com>
2024-09-07 22:56:09 +0200

    fix database use

--- [core]/player/server/src/index.ts
+++ [core]/player/server/src/index.ts
@@ -1,8 +1,5 @@
-import { prisma } from "database"
+import { prisma } from "@lib/database"
 
-prisma.player.findMany().then(
-  (players: any) => { console.log("players", players) }
-)
 /*
 on("onResourceStart", async (resName: string) => {
   if (resName === GetCurrentResourceName()) {
@@ -11,9 +8,12 @@ on("onResourceStart", async (resName: string) => {
     console.log("players", JSON.stringify(players))
   }
 });
+*/
 
 onNet("player:get-players:request", async (cb: Function) => {
   const players = await prisma.player.findMany();
 
-  emitNet("player:get-players:response", JSON.stringify(players))
-});*/
+  await prisma.player.findMany().then(
+    (players: any) => { console.log("players", players) }
+  )
+});
